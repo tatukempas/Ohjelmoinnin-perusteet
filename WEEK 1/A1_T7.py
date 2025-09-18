0b696283e076cf9667bcc4ccbac6bb2bf6347d17
tatukempas <tatu.kempas@student.lab.fi>
2025-09-18 18:03:01 +0300

    Update A1_T7.py

--- WEEK 1/A1_T7.py
+++ WEEK 1/A1_T7.py
@@ -3,6 +3,6 @@ Feed = input("Enter travel distance(kilometers): ")
 Distance = int(Feed)
 input("Enter fuel usage(liters): ")
 FuelUsage = int(Feed)
-Consumption = 200 / 20
+Consumption = Distance / FuelUsage
 Consumption = int(Consumption)
 print("Fuel consumption is", Consumption, "l per 100km")
