ca4af2bb4608240e0e6d3a94c8f148cfdf578bd0
Tatu Kempas <tatu.kempas@student.lab.fi>
2025-11-20 10:40:35 +0200

    viikko 6 korjattu

--- WEEK 6/A6_T5.py
+++ WEEK 6/A6_T5.py
@@ -1,42 +1,19 @@
-SEPARATOR = ";"
-
-def readValues(filename: str) -> str:
+def readValues(filename: str):
+    values = []
     with open(filename, "r", encoding="UTF-8") as f:
-        lines = f.readlines()
-    values = ""
-    for line in lines:
-        number = line.strip()
-        if values == "":
-            values += number
-        else:
-            values += SEPARATOR + number
+        for line in f:
+            row = line.strip()
+            if row != "":
+                values.append(int(row))
     return values
 
-def analyseNumbers(values_str: str) -> str:
-    parts = values_str.split(SEPARATOR)
-    numbers = [int(x) for x in parts]
-    count = len(numbers)
-    total = sum(numbers)
-    greatest = max(numbers)
+
+def analyseValues(values):
+    count = len(values)
+    total = sum(values)
+    greatest = max(values)
     average = total / count
-    results = f"{count}{SEPARATOR}{total}{SEPARATOR}{greatest}{SEPARATOR}{average:.2f}"
-    return results
 
-def displayResults(filename: str, results: str):
-    print("#### Number analysis - START ####")
-    print(f'File "{filename}" results:')
-    print("Count;Sum;Greatest;Average")
-    print(results)
-    print()
-    print("#### Number analysis - END ####")
-
-def main():
-    print("Program starting.")
-    filename = input("Insert filename: ")
-    values_str = readValues(filename)
-    results = analyseNumbers(values_str)
-    displayResults(filename, results)
-    print("Program ending.")
-
-if __name__ == "__main__":
-    main()
+    result = "Count;Sum;Greatest;Average\n"
+    result += "{};{};{};{:.2f}\n".format(count, total, greatest, average)
+    return result
